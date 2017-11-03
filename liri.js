d93ac69d6a2ed6778c43e44de0c0a9104a86340a
cclarkcode <cclarkrun@gmail.com>
2017-11-03 15:16:41 -0700

    Added better commenting

--- liri.js
+++ liri.js
@@ -11,6 +11,8 @@ var fs = require('fs');
 var command = process.argv[2];
 var clidata = process.argv.slice(3).join(' ');
 
+
+//Object function with commany keys
 var actions = {
 	'my-tweets': myTweets,
 	'spotify-this-song': spotify,
@@ -29,9 +31,10 @@ else {
 }
 
 
-
+//Function to handle all calls to twitter API
 function myTweets (handle) { 
 
+	//Default logic, output variable for simplified console.log + log file outputting
 	if (handle === '') {
 		handle='pattonoswalt';
 		var output = '\n\nGetting tweets for ' + handle + '(default)\n\n';
@@ -40,7 +43,7 @@ function myTweets (handle) {
 		var output = '\n\nGetting tweets for ' + handle + '\n\n';
 	}
 
-
+	//Create twitter client with specified keys
 	var client = new Twitter({
 	consumer_key: twitterKeys.consumer_key,
 	consumer_secret: twitterKeys.consumer_secret,
@@ -51,12 +54,14 @@ function myTweets (handle) {
 
 var params = {screen_name: handle};
 
+//Call Twitter api
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
      for (var i = 0; i < tweets.length; i++) {
      	var date = tweets[i].created_at.substr(4,15);
      	output+= date + ': ' + tweets[i].text + '\n';
      }
+     //Send output to console and to log file
      console.log(output);
      write(output);
   }
@@ -67,7 +72,10 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 }
 
+//Call spotify api
 function spotify(song) {
+
+	//Default logic, output variable for simplified console.log + log file outputting
 	if (song==='') {
 		song="The Sign Ace of Base";
 		var output = "Searching for " + song + '(default)';
@@ -76,11 +84,13 @@ function spotify(song) {
 		var output = 'Searching for ' + song;
 	}
 
+	//Create Spotify client with specified authorization keys
 	var spotifyApi = new SpotifyWebApi({
 		clientId : spotifykeys.client_ID,
   		clientSecret : spotifykeys.client_secret
 	});
 	
+	//Call spotify API
 	spotifyApi.clientCredentialsGrant()
 	  .then(function(data) {
 	    
@@ -91,13 +101,17 @@ function spotify(song) {
 		spotifyApi.searchTracks(song)
 		  .then(function(data) {
 		  	
+		  	//Create output for console and log file
 		    output+='\n\nArtist(s): ' + data.body.tracks.items[0].artists[0].name + '\n';
 		    output+='Song name: ' + data.body.tracks.items[0].name +'\n';
 		    output+='Album: ' + data.body.tracks.items[0].album.name + '\n';
 		    output+='Link to song: ' + data.body.tracks.items[0].external_urls.spotify+ '\n';
 		    output+='\nIf you didn\'t get the song you were looking for, try adding the artist\'s name to the command line search\n';
+		    
+		    //Write output to console and log file
 		    console.log(output);
 		    write(output);
+
 		  }, function(err) {
 		    console.error(err);
 		  });
@@ -112,15 +126,23 @@ function spotify(song) {
 
 function movieapi (movie) {
 
+	//Handle default logic, output variable for simplified console and log file output
 	if (movie === '') {
 		movie = 'Mr. Nobody';
+		var output = '\nRunning movie search on ' + movie + '(default)\n';
+	}
+	else {
+		var output = '\nRunning movie search on ' + movie + '\n';
 	}
 
+	//Create url for OMDB api call
 	var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
+	//Make api call
 	request(queryURL, function (error, response, body) {
 	  	var movieobject = JSON.parse(body);
-	  	var output = '\nRunning movie search on ' + movie;
+	  	
+	  	//Create output for console and log file
 	  	output+='\nMovie: ' + movieobject.Title + '\n';
 	  	output+='Released: ' + movieobject.Year + '\n';
 	  	output+='IMDB Rating: ' + movieobject.Ratings[0].Value + '\n';
@@ -129,6 +151,8 @@ function movieapi (movie) {
 	  	output+='Synopsis: " ' + movieobject.Plot + '\n';
 	  	output+='Actors: ' + movieobject.Actors + '\n';
 
+
+	  	//Write to console and to log file
 	  	console.log(output);
 	  	write(output);
 	 
@@ -138,20 +162,24 @@ function movieapi (movie) {
 
 }
 
+//Function to run program off text file
 function datafile (filename) {
 
+	//Handle default logic
 	if (filename === '') {
 		filename = 'random.txt';
 	}
 
+	//Read file
 	fs.readFile(filename, "utf8", function(err, data) {
     if (err) {
       return console.log(err);
     }
 
-    
+    //Parse command and data necessary to execute command
     var command = data.substr(0,data.indexOf(','));
     var data = data.substr(data.indexOf(',')+1,data.length).replace(/"/g,'');
+
 
     console.log('\nPerforming ' + command + ' command on ' + data+ '\n\n');
     
@@ -166,9 +194,11 @@ function datafile (filename) {
   });
 }
 
-
+//Function which displays all available commands with instructions
+//Function runs by default if invalid command given
 function help (command) {
 
+	//Determines whether help was specified or invalid command given
 	if (command === help) {
 		var output = '\nRunning help command\n';
 	}
@@ -188,12 +218,12 @@ function help (command) {
 	output+='help\n';
 	output+='--Will display these instructions.\n';
 
+	//Write output to console and to log file
 	console.log(output);
-
 	write(output);
 }
 
-
+//Function which writes output to log file
 function write(output) {
 
 	fs.appendFile('log.txt', output,function(err) {
